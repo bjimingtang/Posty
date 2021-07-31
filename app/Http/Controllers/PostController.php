af14ca0be13313e79ad71f08a64cf9b9407477eb
bjimingtang <brendan.jiming.tang@gmail.com>
2021-07-30 18:23:48 -0700

    post creation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() {
-      return view('posts.index');
+      $posts = Post::paginate(10);
+      return view('posts.index', [
+        'posts' => $posts
+      ]);
     }
 
     public function store(Request $request) {
-      
+      $this->validate($request, [
+        'body' => 'required'
+      ]);
+
+      auth()->user()->posts()->create([
+        'body' => $request->body
+      ]);
+
+      return back();
     }
 }

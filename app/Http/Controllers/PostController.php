ee85a83665d592cf0a326af65dbeedecc7824d65
bjimingtang <brendan.jiming.tang@gmail.com>
2021-07-31 18:18:46 -0700

    reduce query count by querying for user and likes together with posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {
-      $posts = Post::paginate(10);
+      $posts = Post::with(['user', 'likes'])->paginate(10);
       return view('posts.index', [
         'posts' => $posts
       ]);
